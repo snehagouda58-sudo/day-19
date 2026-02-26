4ada58ae49a3ff637976d1df5cbd13f40fa9d67d
sneha <snehagouda58@gmail.com>
2026-02-26 12:18:07 +0530

    Removed substraction completely

--- app.py
+++ app.py
@@ -3,9 +3,3 @@ def add(a, b):
 
     if __name__ == "__main__": 
      print("Addition:", add(5, 3)) 
-
-def subtract(a, b): 
-    return a - b 
-
-    if __name__ == "__main__":
-     print("Subtraction:", subtract(5, 3)) 

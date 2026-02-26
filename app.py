64d690e59a7c3ff9046ac8f18ab26afd59170c75
sneha <snehagouda58@gmail.com>
2026-02-26 12:56:29 +0530

    Updated app.py before merging

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 def add(a, b): 
     return a + b 
+def subtract(a, b):
+    return a - b
 
     if __name__ == "__main__": 
-     print("Addition:", add(5, 3)) 
+         print("Addition:", add(5, 3)) 
+         print("Subtraction:", subtract(5, 3))
